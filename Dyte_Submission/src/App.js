ebf97b3fb4a9fe9186b2c91c2b4e2aed4b67a0b2
Prithiraj18bce2512 <66234297+Prithiraj18bce2512@users.noreply.github.com>
2021-07-17 21:32:30 +0530

    Add files via upload

---
+++ Dyte_Submission/src/App.js
@@ -0,0 +1,20 @@
+//Prithiraj Bhuyan - 18BCE2512
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>DYTE Submission</h1>
+      <div>
+        <h2>DYTE Submission - h2</h2>
+        <textarea name="" id="" cols="30" rows="30"></textarea>
+      </div>
+      <div>
+        <h2>Live View</h2>
+        <textarea name="" id="" cols="30" rows="30"></textarea>
+      </div>
+    </div>
+  );
+}
+
+export default App;
